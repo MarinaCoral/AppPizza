bd38bb82333d6fb068e57f1401872c33cf9a2ea5
MarinaCoral <54864108+MarinaCoral@users.noreply.github.com>
2023-10-03 22:26:22 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMeeT6PKAbSsd9d9mb0QUVBwN_n5vxQ4k",
+  authDomain: "senhordaspizzasbd-17baf.firebaseapp.com",
+  projectId: "senhordaspizzasbd-17baf",
+  storageBucket: "senhordaspizzasbd-17baf.appspot.com",
+  messagingSenderId: "958943385591",
+  appId: "1:958943385591:web:55d8127b12d9ceae3e40f2",
+  measurementId: "G-5TFYRZ2LHX"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
